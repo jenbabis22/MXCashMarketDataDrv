e7280c4908ff3cc74d4119b2f4a1e977cfea24ab
Felix Corona <felix@felix-hulke.(none)>
2013-01-24 19:32:02 -0600

    add environment WOMBAT_PATH

--- src/main/scripts/start-driver.sh
+++ src/main/scripts/start-driver.sh
@@ -4,14 +4,15 @@
 #
 # start-driver.sh
 #
-# USAGE: start-driver.sh
+# USAGE: start-driver.sh <port driver>
 #
 # DESCRIPTION: Runs the market data driver
 #
 #  See COPYING For license
 #===================================================================================
 
+#export WOMBAT_PATH=../config
 
 export JAVA_OPTS=" -Xms512m -Xmx1024m -XX:MaxPermSize=512m "
 
-java -jar ../mx-cash-marketdata-driver.jar -port_mina 1656
+java -jar ../mx-cash-marketdata-driver.jar -port_mina $1
