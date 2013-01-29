ef5aa13cd4ed05d9cf0492323a2750e349c8ea15
Felix Corona <felix@felix-hulke.(none)>
2013-01-28 18:38:39 -0600

    writing correction in a binary file

--- src/main/java/com/interacciones/mxcashmarketdata/driver/util/AppropriateFormat.java
+++ src/main/java/com/interacciones/mxcashmarketdata/driver/util/AppropriateFormat.java
@@ -20,14 +20,13 @@
 
 package com.interacciones.mxcashmarketdata.driver.util;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
-
 
 import com.interacciones.mxcashmarketdata.driver.model.MessageRetransmission;
 
@@ -44,12 +43,12 @@ public class AppropriateFormat {
 	private static InputStreamReader isr = null;
 	private static BufferedReader br = null;
 
+	private static File filewrite = null;
 	private static FileOutputStream fos = null;
-	private static ObjectOutputStream oos = null;
+	private static BufferedOutputStream bos= null;
 	
 	private static String source = "30122010.txt";
 	private static String destination = "converterBinary";
-	//private static String path = "";
 	
 	public static void main(String[] args) throws Throwable {
 		init(args);
@@ -63,8 +62,9 @@ public class AppropriateFormat {
 	            isr = new InputStreamReader(is);
 		 	    br = new BufferedReader(isr);
 		 	    //write
-			    fos = new FileOutputStream(destination);
-				oos = new ObjectOutputStream(fos);
+		 	    filewrite = new File(destination);
+		 	    fos = new FileOutputStream(filewrite);
+		 	    bos = new BufferedOutputStream(fos);
 
 		 	    int data = 0;
 			    int count = 0;
@@ -80,12 +80,12 @@ public class AppropriateFormat {
 			            message.delete(INIT_ASCII,message.length());
 			        }
 			    }
-	            //break;
 	        } catch (Exception e) {
 	            LOGGER.error("Warning " + e.getMessage());
 	            e.printStackTrace();
+	        } finally {
+	        	close();
 	        }
-	        close();
 	    //}
 	    time = System.currentTimeMillis() - time;
 	    LOGGER.info("Time " + time);
@@ -109,8 +109,7 @@ public class AppropriateFormat {
 	}
 	
 	private static void writeToFile(byte[] message) throws IOException {
-		oos.write(message);
-		oos.flush();
+		bos.write(message);
 	}
 	
 	private static void init(String[] args){
@@ -135,9 +134,7 @@ public class AppropriateFormat {
 	
 	private static void close() throws IOException{
 		//write
-		oos.flush();
-		oos.close();
-		fos.close();
+		bos.close();
 		
 		//read
 		br.close();
